58fa4b678223fe36ee7f7b487be2f062b31e2472
Maxim Bratash <maxim.bratash@gmail.com>
2016-03-14 13:57:10 +0200

    Added ch2 ex2,3 and started ex4

---
+++ src/com/mrgnstrn/chapter2/Example2/DataTypes.java
@@ -0,0 +1,34 @@
+package com.mrgnstrn.chapter2.Example2;
+
+/**
+ * Created by mrgns on 14.03.2016.
+ */
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 1, b1 = 1+2;
+        final byte B = 1 + 2;
+        b = (byte)(b1 + 1);
+        b = B + 1;
+
+        int i = 3;
+        b = (byte)i;
+
+        final int I = 2;
+        b = I;
+
+        b = 127;
+        System.out.println(b) ;
+        i = b + 12;
+
+        System.out.println(i);
+
+       i  = 4 > 5 ? 1 : 0;
+        System.out.println(i);
+        i  = 4 > 3 ? 1 : 0;
+        System.out.println(i);
+
+
+    }
+
+
+}
---
+++ src/com/mrgnstrn/chapter2/Example4/AutoboxingRunner.java
@@ -0,0 +1,7 @@
+package com.mrgnstrn.chapter2.Example4;
+
+/**
+ * Created by mrgns on 14.03.2016.
+ */
+public class AutoboxingRunner {
+}
